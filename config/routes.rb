cf0531ef08f4becddcab2befd473694ff8a6a933
= <margaryan.mher.28@gmail.com>
2016-11-27 20:43:20 +0400

    Rating funcionality added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :movies
+
+  resources :movies do
+    resources :reviews, excrept: [:show, :index]
+  end
+
   root 'movies#index'
 end
